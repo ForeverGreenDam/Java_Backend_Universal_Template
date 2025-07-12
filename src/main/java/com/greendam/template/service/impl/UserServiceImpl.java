bf0b86556105831bf5f87726590d8260f8bf3dd3
ForeverGreenDam <2985601245@qq.com>
2025-07-12 20:06:15 +0800

    增加了一个service注解

--- src/main/java/com/greendam/template/service/impl/UserServiceImpl.java
+++ src/main/java/com/greendam/template/service/impl/UserServiceImpl.java
@@ -1,10 +1,12 @@
 package com.greendam.template.service.impl;
 
 import com.greendam.template.service.UserService;
+import org.springframework.stereotype.Service;
 
 /**
  * 用户服务实现类
  * @author ForeverGreenDam
  */
+@Service
 public class UserServiceImpl implements UserService {
 }
