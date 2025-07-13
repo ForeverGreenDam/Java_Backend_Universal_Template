05dde1ccc939b243bc37fdc8ca498339285c6b6a
ForeverGreenDam <2985601245@qq.com>
2025-07-13 17:32:19 +0800

    引入OSS对象存储

---
+++ src/main/java/com/greendam/template/config/OssConfiguration.java
@@ -0,0 +1,26 @@
+package com.greendam.template.config;
+
+import com.greendam.template.properties.AliOssProperties;
+import com.greendam.template.common.utils.AliOssUtil;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * 阿里OSS配置类，用于创建AliOssUtil对象
+ */
+@Configuration
+@Slf4j
+public class OssConfiguration {
+
+    @Bean
+    @ConditionalOnMissingBean //确保只有一个AliOssUtil实例
+    public AliOssUtil aliOssUtil(AliOssProperties aliOssProperties) {
+        log.info("开始创建阿里文件上传类对象:{}", aliOssProperties);
+        return new AliOssUtil(aliOssProperties.getEndpoint(),
+                aliOssProperties.getAccessKeyId(),
+                aliOssProperties.getAccessKeySecret(),
+                aliOssProperties.getBucketName());
+    }
+}
